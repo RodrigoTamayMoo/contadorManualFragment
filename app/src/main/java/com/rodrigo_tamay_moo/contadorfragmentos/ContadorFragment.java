3048d929231be95a1d1244957a86e90726cf31ab
RodrigoTamayMoo <14470439@itcampeche.edu.mx>
2017-10-29 20:05:27 -0600

    ContadorFragmento: el fragmento de interfaz de usuario

---
+++ app/src/main/java/com/rodrigo_tamay_moo/contadorfragmentos/ContadorFragment.java
@@ -0,0 +1,10 @@
+package com.rodrigo_tamay_moo.contadorfragmentos;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by rodrigo_tamay_moo on 29/10/2017.
+ */
+
+public class ContadorFragment extends Fragment {
+}
